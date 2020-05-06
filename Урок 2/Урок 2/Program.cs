4a14791b009597a0ebf3e29ffc0b11c1072827f3
PavlenkoPavel <paxan2546@gmail.com>
2020-05-06 06:36:58 +0300

    Выполнил задание 1 2 и 4. Сделал все в одном рещении урок2

---
+++ Урок 2/Урок 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+    class Program
+{
+    static int MinNum()
+    //Написать метод, возвращающий минимальное из трех чисел. Павленко
+    {
+        Console.WriteLine("Введите три целых числа: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int min = a;
+        if (b < min) min = b;
+        if (c < min) min = c;
+        return min;
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Наименьшее из трех введенных чисел равно {MinNum()}");
+        Console.ReadLine();
+    }
+}
---
+++ урок 2. задание 2/урок 2. задание 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Program
+//Написать метод подсчета количества цифр числа. Павленко
+{
+    static void Main()
+    {
+        Console.Write("Число: ");
+        Console.WriteLine("Количество знаков: " + count(Console.ReadLine()));
+        Console.ReadKey();
+    }
+
+    static int count(string s)
+    {
+        return s.Length;
+    }
+}
+ 
+
